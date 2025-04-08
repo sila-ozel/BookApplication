d2e38a21c3fd3129ba75e918ed151ccb066b32c6
sila-ozel <ozelsla03@gmail.com>
2025-04-08 06:31:49 +0000

    New model 'Book' added. (Human generated commit message)

---
+++ login/src/main/java/com/sila/login/model/Book.java
@@ -0,0 +1,86 @@
+package com.sila.login.model;
+
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    private LocalDate publishedDate;
+
+    // Constructors
+    public Book() {}
+
+    public Book(String title, String author, String isbn, LocalDate publishedDate) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedDate = publishedDate;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    // Optional: toString, equals, and hashCode methods
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publishedDate=" + publishedDate +
+                '}';
+    }
+}

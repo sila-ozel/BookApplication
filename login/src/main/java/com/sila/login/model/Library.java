888e2ad37695641237d5736dd808e69991e0bde2
sila-ozel <ozelsla03@gmail.com>
2025-04-08 13:01:20 +0000

    Added Library entity class for managing library data in the application. This class includes fields for the library's name, location, and a list of associated books. It also provides constructors, getters, setters, utility methods for adding and removing books, and a toString method for easy representation.

---
+++ login/src/main/java/com/sila/login/model/Library.java
@@ -0,0 +1,83 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "libraries")
+public class Library {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "library_id") // This adds a foreign key in the Book table
+    private List<Book> books = new ArrayList<>();
+
+    // Constructors
+    public Library() {}
+
+    public Library(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    // Utility methods
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    // Optional: toString
+    @Override
+    public String toString() {
+        return "Library{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}

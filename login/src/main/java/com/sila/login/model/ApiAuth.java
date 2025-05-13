3cfd2a57c19b4f650aef5406010e3f819f5c6891
sila-ozel <ozelsla03@gmail.com>
2025-05-13 18:21:50 +0000

    Refactor ApiAuth class

--- login/src/main/java/com/sila/login/model/ApiAuth.java
+++ login/src/main/java/com/sila/login/model/ApiAuth.java
@@ -1,17 +1,23 @@
 package com.sila.login.model;
-// adding a comment
-public class ApiAuth {
-    private String username, password, age;
 
-    public ApiAuth() {}
+// Class to hold API login authentication data
+public class ApiAuth {
+    private String username;
+    private String password;
+    private int age; // corrected from String to int for consistency
+
+    public ApiAuth() {
+        // Default constructor
+    }
 
     public ApiAuth(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    // Getters and Setters
     public String getUsername() {
-        return this.username;
+        return username;
     }
 
     public void setUsername(String username) {
@@ -19,13 +25,18 @@ public class ApiAuth {
     }
 
     public String getPassword() {
-        return this.password;
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
+
     public int getAge() {
-        return this.age;
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
